f464dada3412192e697b7a1bd5e9d0cb82027891
SHANE MURPHY - Student <G00327009@gmit.ie>
2017-10-02 11:24:04 +0100

    Finished node lab

---
+++ node-v8.4.0-win-x64/main.js
@@ -0,0 +1,17 @@
+var http = require("http");
+http.createServer(function (request, response) {
+
+ response.writeHead(200, {'Content-Type': 'text/plain'});
+
+
+ response.end('Data representation and Querying');
+}).listen(8081);
+
+console.log('Server running at http://127.0.0.1:8081/');
+
+var fs = require("fs");
+fs.readFile('datarep.txt', function (err, data) {
+ if (err) return console.error(err);
+ console.log(data.toString());
+});
+console.log("Program Ended");
